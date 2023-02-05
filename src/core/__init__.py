23ec0ab3bfa545dfd7671e7ec5b016d40f975294
nebelfox <mashtaliaryaroslav@gmail.com>
2023-02-05 20:15:04 +0200

    Update core module with plugins-related aliases

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,4 +2,9 @@ from .config import Config
 from .assets import Assets, FileAssets, yaml_assets, txt_assets
 from .chat import Chat
 from .help import Help
+from .plugin import Plugin
+
+plugin = Plugin.register_plugin
+plugins = Plugin.plugins
+
 from .bot import Bot
