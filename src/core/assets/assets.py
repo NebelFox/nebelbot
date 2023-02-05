a42acce55a758f182540cd6cef88d0a29f23495a
nebelfox <mashtaliaryaroslav@gmail.com>
2023-02-05 19:34:39 +0200

    Add abstract Assets class

---
+++ src/core/assets/assets.py
@@ -0,0 +1,18 @@
+from typing import Any
+from abc import ABC, abstractmethod
+
+
+class Assets(ABC):
+    @abstractmethod
+    def get(self, key: str) -> Any:
+        pass
+
+    @abstractmethod
+    def set(self, key: str, value: Any):
+        pass
+
+    def __getitem__(self, key: str) -> Any:
+        return self.get(key)
+
+    def __setitem__(self, key: str, value: Any):
+        self.set(key, value)
